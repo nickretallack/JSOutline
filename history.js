d6e0ee70e1ce9fa54b2473ecdc0a026a59d621a7
Nick Retallack <nickretallack@gmail.com>
2009-08-28 18:07:05 -0700

    refactored event storage

--- history.js
+++ history.js
@@ -1,3 +1,56 @@
+function HistoryRunner(storage){
+  var event_count = storage.get('event_count') || 0
+  var current_event = storage.get('current_event') || 0
+
+  function store_meta(count){
+    storage.set('current_event',current_event)
+    if (count) storage.set('event_count', event_count)    
+  }
+  
+  this.doit = function(data){
+    storage.set('event-'+current_event, data)
+    event_count = current_event += 1
+    store_meta(true)
+
+    // invalidate undo history
+    for(var x = current_event; x < event_count; x++) storage.unset('event-'+x)
+  }
+  
+  this.undo = function(){
+    if (current_event <= 0) return null
+    current_event -= 1
+    store_meta(false)
+    return storage.get("event-"+current_event)
+  }
+  
+  this.redo = function(){
+    if (current_event >= event_count ) return null
+    var event = storage.get("event-"+current_event)
+    current_event += 1
+    store_meta(false)
+    return event
+  }
+  
+  var restore_counter = 0
+  this.restore = function(){
+    if (restore_counter >= current_event) return null
+    var event = storage.get("event-"+restore_counter)
+    restore_counter += 1
+    return event
+  }
+  
+  this.clear = function(){
+    if (storage.clear) return storage.clear()
+    for(var x = 0; x < event_count; x++) storage.unset("event-"+x)
+    storage.unset('event_count')
+    storage.unset('current_event')
+  }
+  
+  this.space = storage.space()
+  this.name = storage.name
+}
+
+
 // These really need some better names
 // var events = []
 // var undone_events = []
@@ -55,3 +108,23 @@ var redo_events = {
   
   
 }
+
+function restore(persistence){
+  history_mode = true
+  while (true){
+    var event = persistence.restore()
+    if (event) forward_events[event.type](event)
+    else break    
+  }
+  history_mode = false
+}
+
+function check_cookies(){
+  $.cookie('test',null)
+  $.cookie('test','true')
+  if (!$.cookie('test')) return false
+  $.cookie('test',null)
+  return true
+}
+
+
